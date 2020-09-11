298a29eb155beaa67dc83ecad3c1bd2cb4edf3a1
alen-sljivar <alen.sljivar@five.agency>
2020-09-12 00:41:23 +0200

    add handleSubmit to Searchaccount component. add account state to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { ThemeProvider } from 'emotion-theming';
-import { CURRENT_USER, User, GET_ACC_REPOSITORIES } from './api/queries';
-import { Layout } from './common';
+import { CURRENT_USER, User, GET_ACC_REPOSITORIES, GET_ACCOUNT } from './api/queries';
+import { Layout, Loader } from './common';
 import { Global } from '@emotion/core';
 import { SearchAccount, Profile, Repositories } from './components';
 import { theme, GlobalStyles, Text } from './theme';
@@ -10,26 +10,36 @@ import { theme, GlobalStyles, Text } from './theme';
 import 'semantic-ui-css/semantic.min.css';
 
 const App = () => {
-  const { loading: loadingUser, data: userData } = useQuery<User>(CURRENT_USER);
-  const { loading: loadingRepository, data: repositoriesData } = useQuery(GET_ACC_REPOSITORIES, {
+  // const { loading: loadingUser, data: userData } = useQuery<User>(CURRENT_USER);
+  // const { loading: loadingRepository, data: repositoriesData } = useQuery(GET_ACC_REPOSITORIES, {
+  //   variables: {
+  //     number_of_repos: 5,
+  //   },
+  // });
+
+  const [account, setAccount] = useState('');
+
+  const { loading: loadingAccount, data: accountData } = useQuery(GET_ACCOUNT, {
     variables: {
-      number_of_repos: 5,
+      username: account,
     },
   });
 
-  if (loadingUser || loadingRepository) return <div>Loading</div>;
+  console.log('account', account);
+
   return (
     <>
       <Global styles={GlobalStyles} />
       <ThemeProvider theme={theme}>
+        <Loader active={loadingAccount} />
         <Layout.Flex>
           <Text color="white" fontFamily="inherit">
             Github profiles app
           </Text>
-          <SearchAccount />
-          <span>{JSON.stringify(userData)}</span>
-          {/* <Profile /> */}
-          <Repositories data={repositoriesData} />
+          <SearchAccount submitAccount={setAccount} />
+          {/* <span>{JSON.stringify(userData)}</span> */}
+          <Profile profileData={accountData} />
+          {/* <Repositories data={repositoriesData} /> */}
         </Layout.Flex>
       </ThemeProvider>
     </>
--- src/components/SearchAccount.tsx
+++ src/components/SearchAccount.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { PrimaryButton, Layout, SearchInput } from '../common';
 
 const { Flex, Spacer } = Layout;
 
-const SearchAccount = () => {
+const SearchAccount = ({ submitAccount }: any) => {
+  const ref = useRef();
+
+  const handleChange = (event) => {
+    ref.current = event.target.value;
+  };
+
+  const handleSubmit = () => {
+    submitAccount(ref.current);
+  };
+
   return (
     <Flex flexDirection="column" alignItems="center" width="70%" mx="auto" py="2rem">
       <Flex>
-        <SearchInput inverted />
+        <SearchInput inverted onChange={handleChange} />
         <Spacer mr={10} />
-        <PrimaryButton>Search Repository</PrimaryButton>
+        <PrimaryButton onClick={handleSubmit}>Search Repository</PrimaryButton>
       </Flex>
     </Flex>
   );
