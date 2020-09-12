92f65431750af8a38bcf6a7bcd56027c03b6c5da
alen-sljivar <alen.sljivar@five.agency>
2020-09-12 16:35:18 +0200

    Add Items to repositories component

--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Item, Card } from 'semantic-ui-react';
 import { Node } from '../api/types';
 
 const Repositories: React.FC<{ repositoriesData: Node[] }> = ({ repositoriesData }) => {
-  console.log('repositoriesData', repositoriesData);
-  return <div />;
+  return (
+    <Item.Group divided>
+      {repositoriesData.map(({ name, url, description }) => {
+        return (
+          <Card>
+            <Item.Content>
+              <Item.Header>{name}</Item.Header>
+              <Item.Meta>{description}</Item.Meta>
+              <Item.Extra as="a">{url}</Item.Extra>
+            </Item.Content>
+          </Card>
+        );
+      })}
+    </Item.Group>
+  );
 };
 
 export default Repositories;
