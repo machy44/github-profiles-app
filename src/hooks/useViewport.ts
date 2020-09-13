356ad6d61d967c1da2a0645180eefb58fe264b2e
alen-sljivar <alen.sljivar@five.agency>
2020-09-13 20:00:06 +0200

    add lodash.debounce and debounce window resizing

--- src/hooks/useViewport.ts
+++ src/hooks/useViewport.ts
@@ -1,12 +1,13 @@
 import { useEffect, useState } from 'react';
 import { SMALL_SCREEN } from '../consts';
+import _debounce from 'lodash.debounce';
 
 // https://blog.logrocket.com/developing-responsive-layouts-with-react-hooks/
 const useViewport = () => {
   const [width, setWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    const handleWindowResize = () => setWidth(window.innerWidth);
+    const handleWindowResize = _debounce(() => setWidth(window.innerWidth), 100);
     window.addEventListener('resize', handleWindowResize);
     return () => window.removeEventListener('resize', handleWindowResize);
   }, []);
